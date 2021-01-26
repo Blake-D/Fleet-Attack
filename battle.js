479aaf8733e3ad282f85ef0f36ae4069add0a57c
Blake DeGraw <bbdegraw@gmail.com>
2021-01-26 13:33:16 -0800

    battle logic template started

---
+++ battle.js
@@ -0,0 +1,27 @@
+const fleet = [
+    {
+        name: 'ship0',
+        power: 78462,
+        role: 'Attacker'
+    },
+    {
+        name: 'ship1',
+        power: 75288,
+        role: 'Support'
+    }, 
+    {
+        name: 'ship2',
+        power: 45142,
+        role: 'Attacker'
+    },
+    {
+        name: 'ship3',
+        power: 83759,
+        role: 'Capital Ship'
+    },
+    {
+        name: 'ship4',
+        power: 71758,
+        role: 'Tank'
+    }
+]
